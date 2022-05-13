7c5949a970336d3b2edb8d2fe19880f0606c19b8
Morgan Riley <MJGRiley@gmail.com>
2022-05-13 13:48:25 -0700

    plane with random verticies moved slightly

--- main.js
+++ main.js
@@ -1,117 +1,174 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DoubleSide } from "three";
+import * as dat from "dat.gui";
+import gsap from "gsap";
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const scene = new THREE.Scene();
+const mouse = { x: undefined, y: undefined };
+const raycaster = new THREE.Raycaster();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-})
+  canvas: document.querySelector("#bg"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+camera.position.setZ(5);
 
-renderer.setPixelRatio( window.devicePixelRatio )
-renderer.setSize( window.innerWidth, window.innerHeight )
-renderer.setPixelRatio(devicePixelRatio)
-camera.position.setZ(5)
-
-const boxWidth = 1; //cube
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
-// const radius = 1;  // ui: radius
-// const detail = 2;  // ui: detail
-// const geometry = new THREE.DodecahedronGeometry(radius, detail)
-function makeInstance(geometry, color, x) {
-  const material = new THREE.MeshPhongMaterial({color,shininess: 75});
-  
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  
-  cube.position.x = x;
-  cube.position.y = -2;
-  cube.position.z = -2;
-  return cube;
+//dat.gui
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 20,
+    height: 20,
+    widthSegments: 17,
+    heightSegments: 17,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 20).onChange(generatePlane);
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position; // destructuring
+  for (let i = 0; i < array.length; i += 3) {
+    const z = array[i + 2];
+    array[i + 2] = z - Math.random();
+  }
+  const colors = [];
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4);
+  }
+  planeMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
 }
-function makeMetalInstance(geometry, color, x) {
-  const material = new THREE.MeshStandardMaterial({color, metalness: .95});
-  
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  
-  cube.position.x = x;
-  cube.position.y = 2;
-  cube.position.z = -2;
-  return cube;
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 25, 25);
+const planeMat = new THREE.MeshPhongMaterial({
+  side: DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMat);
+scene.add(planeMesh);
+
+// vertice position randomization 
+const { array } = planeMesh.geometry.attributes.position; // destructuring
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+  array[i] = x  - (Math.random()*.15 );
+  array[i + 1] = y - (Math.random()*.15);
+  array[i + 2] = z - (Math.random()*.15);
 }
-function makePhysicalInstance(geometry, color, x) {
-  const material = new THREE.MeshPhysicalMaterial({color, roughness:1, metalness: .95, clearcoat: 1, });
-  
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  
-  cube.position.x = x;
-  cube.position.z = -2;
-  return cube;
+planeMesh.geometry.attributes.position.originialPosition = planeMesh.geometry.attributes.position.array
+
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(0, 0.19, 0.4);
 }
-const cubes = [
-  makeInstance(geometry, 0x44aa88,  0),
-  makeInstance(geometry, 0x8844aa, -2),
-  makeInstance(geometry, 0xaa8844,  2),
-]
-const metalCubes = [
-  makeMetalInstance(geometry, 0x44aa88,  0),
-  makeMetalInstance(geometry, 0x8844aa, -2),
-  makeMetalInstance(geometry, 0xaa8844,  2),
-]
-const physicalCubes = [
-  makePhysicalInstance(geometry, 0x44aa88,  0),
-  makePhysicalInstance(geometry, 0x8844aa, -2),
-  makePhysicalInstance(geometry, 0xaa8844,  2),
-]
+planeMesh.geometry.setAttribute(
+  "color",
+  new THREE.BufferAttribute(new Float32Array(colors), 3)
+);
 
-const planeGeometry = new THREE.PlaneGeometry(5,5,10,10)
-const planeMat = new THREE.MeshBasicMaterial({ color: 0xff0000})
-const planeMesh = new THREE.Mesh( planeGeometry, planeMat)
-scene.add(planeMesh)
-planeMesh.position.z = -2
+//Lights
+const pointLight = new THREE.DirectionalLight(0xffffff, 1);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
 
-const color = 0xFFFFFF;
-const intensity = 1;
-const pointLight = new THREE.DirectionalLight(color, intensity)
-pointLight.position.set(5,5,5)
-scene.add(pointLight)
+//Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(50, 100);
+scene.add(lightHelper, gridHelper);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(50,100)
-scene.add(lightHelper,gridHelper)
-
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate(time) {
-  requestAnimationFrame( animate )
-  time *= 0.001;  // convert time to seconds
-  const allCubes = cubes.concat(metalCubes).concat(physicalCubes)
- 
-  allCubes.forEach((cube, ndx) => {
-    const speed = 1 + ndx * .1;
-    const rot = time * speed;
-    cube.rotation.x = rot;
-    cube.rotation.y = rot;
-  })
+  requestAnimationFrame(animate);
+  time *= 0.001; // convert time to seconds
 
-  controls.update()
+  controls.update();
 
-  renderer.render( scene, camera )
+  renderer.render(scene, camera);
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(planeMesh);
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes;
+    color.setX(intersects[0].face.a, 0.1); //R
+    color.setY(intersects[0].face.a, 0.5); //G
+    color.setZ(intersects[0].face.a, 1); //B
+
+    color.setX(intersects[0].face.b, 0.1);
+    color.setY(intersects[0].face.b, 0.5);
+    color.setZ(intersects[0].face.b, 1);
+
+    color.setX(intersects[0].face.c, 0.1);
+    color.setY(intersects[0].face.c, 0.5);
+    color.setZ(intersects[0].face.c, 1);
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4,
+    };
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1,
+    };
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      onUpdate: () => {
+        color.setX(intersects[0].face.a, hoverColor.r); //R
+        color.setY(intersects[0].face.a, hoverColor.g); //G
+        color.setZ(intersects[0].face.a, hoverColor.b); //B
+
+        color.setX(intersects[0].face.b, hoverColor.r);
+        color.setY(intersects[0].face.b, hoverColor.g);
+        color.setZ(intersects[0].face.b, hoverColor.b);
+
+        color.setX(intersects[0].face.c, hoverColor.r);
+        color.setY(intersects[0].face.c, hoverColor.g);
+        color.setZ(intersects[0].face.c, hoverColor.b);
+      },
+    });
+
+    color.setX(0, 0).needsUpdate = true;
+  } //intersects[0].face
 }
-
-function onWindowResize() {// If the window gets resized
+function onWindowResize() {
+  // If the window gets resized
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  
-  renderer.setSize( window.innerWidth, window.innerHeight )
-  renderer.setPixelRatio(devicePixelRatio)
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(devicePixelRatio);
 }
+function onMouseMove(e) {
+  mouse.x = (e.clientX / innerWidth) * 2 - 1; //normalizes coordinates to
+  mouse.y = -(e.clientY / innerHeight) * 2 + 1; //match with Three.js
+}
+//Window Events
+window.addEventListener("resize", onWindowResize, false);
+addEventListener("mousemove", onMouseMove);
 
-window.addEventListener('resize',onWindowResize, false)
-
-animate()
+animate();
